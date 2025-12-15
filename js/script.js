380aabd624a2c926e747b63fcc6b4198beda30fe
Goodplayer9016 <yohans31541@gmail.com>
2025-12-16 04:43:36 +0900

    인덱스,1페이지 뼈대

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 $(document).ready(function(){
 
-  /* ===== 슬라이더 ===== */
+  /* ===== 슬라이더 + DOT ===== */
   let count = 0;
+  const max = 2;
+  let timer = null;
 
-  setInterval(function(){
-    count++;
-    if(count > 2){ count = 0; }
+  function goSlide(n){
+    count = n;
+    $(".train").css("transform","translateX(" + (-1200 * count) + "px)");
+    $(".dot").removeClass("on");
+    $(".dot").eq(count).addClass("on");
+  }
 
-    $(".train").css(
-      "transform",
-      "translateX(" + (-1200 * count) + "px)"
-    );
-  }, 2500);
+  function auto(){
+    timer = setInterval(function(){
+      count++;
+      if(count > max) count = 0;
+      goSlide(count);
+    }, 2500);
+  }
+
+  auto();
+
+  $(".dot").click(function(){
+    clearInterval(timer);
+    goSlide($(this).data("idx"));
+    auto();
+  });
 
 
-  /* ===== 탭메뉴 ===== */
+  /* ===== 탭메뉴(서브페이지용) ===== */
   $(".tab_menu li").click(function(){
     let idx = $(this).index();
 
